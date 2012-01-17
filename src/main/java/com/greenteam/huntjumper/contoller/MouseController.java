ab372e8c024ee270c2e9884b7b4af6f9fc093125
BlindButcher <@Alexey.(none)>
2012-01-17 22:02:23 +0200

    --adding mouse accelerating on mouse press-release --renaming some game constants

--- src/main/java/com/greenteam/huntjumper/contoller/MouseController.java
+++ src/main/java/com/greenteam/huntjumper/contoller/MouseController.java
@@ -2,10 +2,13 @@ package com.greenteam.huntjumper.contoller;
 
 import com.greenteam.huntjumper.Camera;
 import com.greenteam.huntjumper.model.Jumper;
-import com.greenteam.huntjumper.utils.GameConstants;
 import com.greenteam.huntjumper.utils.Point;
 import net.phys2d.math.Vector2f;
 import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Input;
+
+import static com.greenteam.huntjumper.utils.GameConstants.*;
+import static java.lang.String.format;
 
 /**
  * Listen and react for mouse and/or keyboard events
@@ -13,7 +16,11 @@ import org.newdawn.slick.GameContainer;
 
 public class MouseController implements IJumperController
 {
+   private static float MIN_IMPULSE = 1.f;
+   private static final float SCALE_INC = 2;
+
    private GameContainer container;
+   private float accumulatedScale = MIN_IMPULSE;
 
    public MouseController(GameContainer container)
    {
@@ -22,13 +29,53 @@ public class MouseController implements IJumperController
 
    public void update(Jumper jumper, int delta)
    {
-      float mouseX = container.getInput().getMouseX();
-      float mouseY = container.getInput().getMouseY();
+      if (accumulating())
+      {
+         return;
+      }
+      else {
+         float mouseX = container.getInput().getMouseX();
+         float mouseY = container.getInput().getMouseY();
+
+         float scale = DEFAULT_FORCE_SCALE;
+         if (releasing()) {
+            scale *= accumulatedScale > MAX_FORCE_SCALE ? MAX_FORCE_SCALE : accumulatedScale;
+            accumulatedScale = MIN_IMPULSE;
+         }
+
+         if (scale > DEFAULT_FORCE_SCALE) System.out.println(format("Force scale is %s", scale));
+         
+         jumper.getBody().addForce(createForce(jumper, scale, mouseX, mouseY));
+
+      }
+   }
+
+   private boolean releasing()
+   {
+      boolean result = false;
+      if (!container.getInput().isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) && accumulatedScale > MIN_IMPULSE)
+      {
+         result = true;
+      }
+      return result;
+   }
+
+   private boolean accumulating() {
+      if (container.getInput().isMouseButtonDown(Input.MOUSE_LEFT_BUTTON))
+      {
+         accumulatedScale += SCALE_INC;
+         return true;
+      }
+      return false;
+   }
+
+   private Vector2f createForce(Jumper jumper, float scale, float mouseX, float mouseY) {
       Vector2f velocity = new Vector2f();
       Point realPoint = Camera.instance().toPhys(new Vector2f(mouseX, mouseY));
       velocity.set((realPoint.getX() - jumper.getBody().getPosition().getX()),
               (realPoint.getY() - jumper.getBody().getPosition().getY()));
-      velocity.scale(GameConstants.JUMPER_FORCE_MULTIPLIER);
-      jumper.getBody().addForce(velocity);
+      velocity.negate();
+      velocity.scale(scale);
+      return velocity;
    }
 }
--- src/main/java/com/greenteam/huntjumper/utils/GameConstants.java
+++ src/main/java/com/greenteam/huntjumper/utils/GameConstants.java
@@ -13,7 +13,8 @@ public final class GameConstants
    
    public static final float JUMPER_RADIUS = 10;
    public static final float JUMPER_MASS = 100;
-   public static final float JUMPER_FORCE_MULTIPLIER = 100;
+   public static final float DEFAULT_FORCE_SCALE = 100;
+   public static final float MAX_FORCE_SCALE = 2000;
 
    public static final float DEFAULT_MAP_RING_RADIUS = 500;
    public static final int DEFAULT_MAP_RING_ANGLES_COUNT = 18;
